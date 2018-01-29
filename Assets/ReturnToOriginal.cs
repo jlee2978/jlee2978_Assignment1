00662320c8f2601de83fa5fd74de9d53d69ee6b5
Jeffrey <jlee2978@uwo.ca>
2018-01-28 20:00:23 -0500

    Initial Commit
    This is my first commit!

---
+++ Assets/ReturnToOriginal.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToOriginal : MonoBehaviour
+{
+    public GameObject instanceObject = null;    // falling object should be bound to this instanceObject
+    public float maxCheckingDistance;  // max distance the ray should check for collisions;
+
+    private Vector3 originalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the original position of each instance of falling objects
+        originalPosition = instanceObject.transform.position;
+
+        if (maxCheckingDistance <= 200)
+        {
+            maxCheckingDistance = 200.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if it is a right mouse click
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Since this script is shared by all the falling objects,
+            // we need to identify the specific object being clicked.
+            GameObject gameObjectGetClicked = GetClickedGameObject();
+
+            // if instanceObject is bound to falling object, gameObjectGetClicked is identified
+            // and theire names are equal, then we can restore to the original position
+            // for this specific object.
+            if (instanceObject != null && gameObjectGetClicked != null && instanceObject.name == gameObjectGetClicked.name)
+            {
+                gameObjectGetClicked.transform.position = originalPosition;
+            }
+        }
+    }
+
+    GameObject GetClickedGameObject()
+    {
+        // Get the clicked object, if any
+
+        // Builds a ray from camera point of view to the mouse position
+        // when a clicking takes place
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Casts the ray and get the first game object hit
+        if (Physics.Raycast(ray, out hit, (float)maxCheckingDistance))
+        {
+            return hit.transform.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    void OnMouseDown()
+    {
+        string objectName = transform.gameObject.name;
+
+        if (objectName.IndexOf("Wall") >= 0)
+        {
+            // if Wall is clicked, get the parent gameObject (i.e. BoxBottom) first
+            // then rotate the parent. We do it with one single statement
+            transform.parent.gameObject.transform.Rotate(new Vector3(-5, 0, -5));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(-5, 0, -5));
+        }
+    }
+}
